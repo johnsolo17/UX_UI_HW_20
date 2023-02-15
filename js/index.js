baa22ca0c8e2dc92a7a809ff8ec11e7fe0f563f8
johnsolo17 <112775094+johnsolo17@users.noreply.github.com>
2023-02-14 22:17:35 -0500

    s

--- js/index.js
+++ js/index.js
@@ -12,3 +12,11 @@ $(function() {
         }
     });
 });
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
